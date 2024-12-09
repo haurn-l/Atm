176319c2fe2276476d4db4e4e1c8ea044201a39f
haurn-l <harunyazirli02@gmail.com>
2024-12-09 16:36:40 +0300

    Update test2.java

--- atm2Deneme/test2.java
+++ atm2Deneme/test2.java
@@ -10,8 +10,8 @@ public class test2 {
 
     public static void main(String[] args) {
         queryIMPL.connection();
-        System.out.println("Hoşgeldiniz");
-        System.out.print("Lütfen hesap şifrenizi giriniz: ");
+        System.out.println("Hosgeldiniz");
+        System.out.print("Lutfen hesap sifrenizi giriniz: ");
         accountPassword = sc.next();
         int option;
         do {
@@ -29,22 +29,22 @@ public class test2 {
 
     public static void menu() {
         System.out.println("MENU");
-        System.out.println("1- BAKİYE SORGULAMA");
+        System.out.println("1- BAKIYE SORGULAMA");
         System.out.println("2- PARA YATIRMA");
         System.out.println("3- PARA ÇEKME");
-        System.out.println("4- TELEFON NUMARASI GÜNCELLEME");
-        System.out.println("5- ŞİFRE GÜNCELLEME");
-        System.out.println("6- HESAP SİL");
-        System.out.println("7- ÇIKIŞ");
+        System.out.println("4- TELEFON NUMARASI GUNCELLEME");
+        System.out.println("5- SIFRE GUNCELLEME");
+        System.out.println("6- HESAP SIL");
+        System.out.println("7- CIKIS");
     }
 
     public static int selectOption() {
-        System.out.print("Yapmak istediğiniz işlem numarasını giriniz: ");
+        System.out.print("Yapmak istediginiz islem numarasını giriniz: ");
         return sc.nextInt();
     }
 
     public static void returnMenu() {
-        System.out.println("Ana menüye dönmek için bir tuşa basınız.");
+        System.out.println("Ana menuye donmek icin bir tusa basinizz.");
         sc.nextLine();
         sc.nextLine();
     }
@@ -56,13 +56,13 @@ public class test2 {
                 System.out.println(balance);
                 break;
             case 2:
-                System.out.print("Yatırılacak miktarı giriniz: ");
+                System.out.print("Yatirilacak miktarı giriniz: ");
                 int deposit = sc.nextInt();
                 balance += deposit;
                 queryIMPL.updateBalance(accountPassword, balance);
                 break;
             case 3:
-                System.out.print("Çekilecek miktarı giriniz: ");
+                System.out.print("Cekilecek miktarı giriniz: ");
                 int withdraw = sc.nextInt();
                 if (balance >= withdraw) {
                     balance -= withdraw;
@@ -72,12 +72,12 @@ public class test2 {
                 }
                 break;
             case 4:
-                System.out.print("Yeni telefon numarasını giriniz: ");
+                System.out.print("Yeni telefon numarasini giriniz: ");
                 String newPhoneNumber = sc.next();
                 queryIMPL.updatePhoneNumber(accountPassword, newPhoneNumber);
                 break;
             case 5:
-                System.out.print("Yeni şifrenizi giriniz: ");
+                System.out.print("Yeni sifrenizi giriniz: ");
                 String newPassword = sc.next();
                 queryIMPL.updateAccountPassword(accountPassword, newPassword);
                 break;
@@ -85,10 +85,10 @@ public class test2 {
                 queryIMPL.removeCostumer(accountPassword);
                 break;
             case 7:
-                System.out.println("Çıkılıyor...");
+                System.out.println("Çikiliyor...");
                 break;
             default:
-                System.out.println("Geçersiz işlem!");
+                System.out.println("Gecersiz islem!");
                 break;
         }
     }
